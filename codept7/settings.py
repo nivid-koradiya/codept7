1daeed73341a6d561c506d58c36eec8e9809379d
nivid-koradiya <ndkoradiya@gmail.com>
2022-07-11 15:35:40 +0530

    added media url and static files

--- codept7/settings.py
+++ codept7/settings.py
@@ -125,3 +125,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# config/settings.py
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
